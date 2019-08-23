4d6b171e4ea3d933f690628d61d761fce7c502c7
Daniel Karski <daniel.karski5q@gmail.com>
2019-08-23 21:55:14 +0200

    Refactore develop:fbinstant package.json command

--- .webpack/fbinstant.js
+++ .webpack/fbinstant.js
@@ -1,17 +1,17 @@
 const webpackMerge = require('webpack-merge');
-const ZipPlugin = require('zip-webpack-plugin');
-const uuidv1 = require('uuid/v1');
+const open = require('open');
 
 const productionConfig = require(`./production.config.js`);
 const commonConfig = require('./common.config.js');
 
 module.exports = () => {
   return webpackMerge(commonConfig, productionConfig, {
-    plugins: [
-      new ZipPlugin({
-        path: `${__dirname}/../archives`,
-        filename: `${uuidv1()}.zip`,
-      }),
-    ],
+    devServer: {
+      after: async function() {
+        await open(
+          `https://www.facebook.com/embed/instantgames/${process.env.FB_APP_ID}/player?game_url=https://localhost:8080`,
+        );
+      },
+    },
   });
 };
